24e548a88cdc8db79245b1594ba7c19d877d28fa
Misha Marinenko <a0x77@icloud.com>
2018-10-21 22:23:32 +0300

    Basic (0000001A)

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style lang="scss">
+$accent-1: #D6CA47;
+$accent-2: #F4F8F8;
+$accent-3: #F4DC13;
+$foreground: #FFFFFF;
+$background: #2A2A33;
+@import "~vue-material/dist/theme/engine"; 
+
+@include md-register-theme("default", (
+  primary: md-get-palette-color($foreground, A200),
+  accent: md-get-palette-color($accent-1, A200), 
+  theme: dark
+));
+
+@import "~vue-material/dist/theme/all"; 
+#app {
+position: absolute;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+background: $background;
+}
+</style>
+
+<style >
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+      <div>
+    <div class="phone-viewport">
+      <md-bottom-bar md-type="shift" :md-theme="'bottom-bar-' + theme">
+        <md-bottom-bar-item md-label="Home" md-icon="home" @click="theme = 'teal'"></md-bottom-bar-item>
+        <md-bottom-bar-item md-label="Pages" md-icon="pages" @click="theme = 'orange'"></md-bottom-bar-item>
+        <md-bottom-bar-item md-label="Posts" md-icon="/assets/icon-whatshot.svg" @click="theme = 'blue'"></md-bottom-bar-item>
+        <md-bottom-bar-item md-label="Favorites" md-icon="favorite" @click="theme = 'red'"></md-bottom-bar-item>
+      </md-bottom-bar>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<script>
+export default {
+  name: 'ChangingThemes',
+  data: () => ({
+    theme: 'teal'
+  })
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
